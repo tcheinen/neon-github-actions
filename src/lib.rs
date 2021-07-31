d06c6b92114ce96689f1615214701d8de63d7e61
Teddy Heinen <teddy@heinen.dev>
2021-07-31 12:54:58 -0500

    does it work now?

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use neon::prelude::*;
 
-fn init(mut cx: FunctionContext) -> JsResult<JsString> {
+fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     println!("howdy from rust");
     Ok(cx.string("hello node"))
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hello", init)?;
+    cx.export_function("hello", hello)?;
     Ok(())
 }
